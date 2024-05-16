023c4ae1265abe5f9890ae926ca58688db3bbc04
Azamat Rashidov <rashiaza@fit.cvut.cz>
2024-05-16 09:45:58 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ProjectRoutes from "./pages/Routes";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ProjectRoutes />
+    </Router>
   );
-}
+};
 
 export default App;
+
+/*const navigate = useNavigate();
+  const auth = useAuth();
+  return auth.isLoggedIn ? (
+    navigate("/login")
+  ) : //
+  auth.user === "owner" ? (
+    <Link to="/ownerproperties">ownerproperties</Link>
+  ) : auth.user === "guest" ? (
+    <Link to="/guesthome">guesthome</Link>
+  ) : auth.user === "inspector" ? (
+    <Link to="/inspector">inspector</Link>
+  ) : (
+    <></>
+  );*/
